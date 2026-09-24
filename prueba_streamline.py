f587facbd13eec2271e20dcbf5ebb57806c8c5f5
cotrapsadesarrollos <cotrapsa.desarrollos@gmail.com>
2026-09-24 13:11:00 -0600

    Refactor file uploader logic for inventory separation

--- prueba_streamline.py
+++ prueba_streamline.py
@@ -16,8 +16,7 @@ inventario_sae = st.file_uploader("Seleccionar Inventario de SAE:", type=["xlsx"
 
 dictio_bases = {}
 for i in range(numero_de_bases):
-    dictio_bases["inventario_base_{0}".format(i)] = st.file_uploader(f"Seleccionar Inventario de la Base {i}:", type=["xlsx","xls"], key=f"Base_{i}")
-    print(dictio_bases["inventario_base_{0}".format(i)].name)
+    dictio_bases["inventario_base_{0}".format(i)] = st.file_uploader(f"Seleccionar Inventario de la Base {i+1}:", type=["xlsx","xls"], key=f"Base_{i}")
 
 if inventario_plataforma is not None and inventario_sae is not None:
     # Leer el archivo Excel del inventario de la plataforma e Inventario SAE
