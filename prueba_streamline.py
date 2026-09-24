ff88112a399cb808ce334fedb43122e632338937
cotrapsadesarrollos <cotrapsa.desarrollos@gmail.com>
2026-09-24 13:05:47 -0600

    Refactor inventory separation logic in Streamlit app

--- prueba_streamline.py
+++ prueba_streamline.py
@@ -16,7 +16,7 @@ inventario_sae = st.file_uploader("Seleccionar Inventario de SAE:", type=["xlsx"
 
 dictio_bases = {}
 for i in range(numero_de_bases):
-    dictio_bases["inventario_base_{0}".format(i)] = st.file_uploader("Seleccionar Inventario de SAE:", type=["xlsx","xls"], element_id=f'base{i}')
+    dictio_bases["inventario_base_{0}".format(i)] = st.file_uploader("Seleccionar Inventario de SAE:", type=["xlsx","xls"])
 
 if inventario_plataforma is not None and inventario_sae is not None:
     # Leer el archivo Excel del inventario de la plataforma e Inventario SAE
